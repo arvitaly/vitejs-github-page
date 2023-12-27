9a1f4027026ed8fd4c411d38b3ea1fde7c1e1d0a
Vitaly <arvitaly@list.ru>
2023-12-28 01:15:52 +0700

    t2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vitejs-github-page/",
   build: {
     outDir: "./docs",
   },
